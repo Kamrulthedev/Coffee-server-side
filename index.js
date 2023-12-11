de9fc42967b7c8c4c3499bce12d58e7aee3f4b8f
Kamrul Hassan <kamrulthedev@gmail.com>
2023-12-11 10:53:07 +0600

    asynk-commit

--- index.js
+++ index.js
@@ -5,21 +5,12 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-
-// maddle ware
+// Middleware
 app.use(cors())
 app.use(express.json())
 
-
-
-
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USERS}:${process.env.DB_PASS}@cluster0.zgseyoe.mongodb.net/?retryWrites=true&w=majority`;
 
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -28,26 +19,39 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+// Connect to MongoDB when the server starts
+client.connect()
+  .then(() => {
+    console.log('Connected to MongoDB');
 
+    const coffeeCollection = client.db('coffeeDB').collection('coffee');
 
-app.get('', (req, res)=>{
-    res.send('coffee maiking in the server')
-})
+    app.post('/coffee', async (req, res) => {
+      const newCoffee = req.body;
+      console.log(newCoffee);
+      
+      try {
+        const result = await coffeeCollection.insertOne(newCoffee);
+        res.json(result);
+      } catch (error) {
+        console.error('Error inserting coffee:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
+    });
 
+    app.get('', (req, res) => {
+      res.send('Coffee making in the server');
+    });
 
-app.listen(port, ()=>{
-    console.log(`coffee server in the running on the, ${port}`)
-})
+    app.listen(port, () => {
+      console.log(`Coffee server is running on port ${port}`);
+    });
+  })
+  .catch(err => console.error('Error connecting to MongoDB:', err));
+
+// Handle clean shutdown
+process.on('SIGINT', async () => {
+  console.log('Shutting down gracefully');
+  await client.close();
+  process.exit(0);
+});
